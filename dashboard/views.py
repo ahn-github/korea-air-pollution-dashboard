d23aa5dee8a3eeabb29ba2d02b592a1cbe051f48
yjucho <hyesung0617@naver.com>
2018-12-27 23:26:39 +0900

    using tf-serving

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from .models import AirKoreaStations, AirKoreaData
-from datetime import datetime as dt
+import datetime as dt
 from djgeojson.views import GeoJSONLayerView
-from datetime import timedelta
-from django.db.models import Count, Max
+from django.db.models import Count
 from django.db.models.functions import Substr
-from keras.models import load_model
 import numpy as np
-from keras import backend as K
+import pandas as pd
+import requests
+import json
 
 def index(request):
-    yesterday = dt.now().replace(microsecond=0,second=0,minute=0, hour=0)
-    num_of_stations = AirKoreaData.objects.filter(datatime__range=(yesterday - timedelta(days=1), yesterday)).\
+    yesterday = dt.datetime.now().replace(microsecond=0,second=0,minute=0, hour=0)
+    num_of_stations = AirKoreaData.objects.filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)).\
             values('stnfk').distinct()
     num_of_failure = AirKoreaStations.objects.exclude(id__in=num_of_stations).values('stationname').count()
-    num_of_polluted = AirKoreaData.objects.filter(datatime__range=(yesterday - timedelta(days=1), yesterday)). \
+    num_of_polluted = AirKoreaData.objects.filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)). \
         filter(khaigrade__gt=2).values('stnfk').distinct().count()
-    num_of_clean = AirKoreaData.objects.filter(datatime__range=(yesterday - timedelta(days=1), yesterday)). \
+    num_of_clean = AirKoreaData.objects.filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)). \
         filter(khaigrade__lt=2). values('stnfk').distinct().count()
 
     context = {
@@ -36,38 +36,40 @@ class MapLayer(GeoJSONLayerView):
 
 
 def detail(request, station_name):
-    yesterday = dt.now().replace(microsecond=0, second=0, minute=0, hour=0)
+    yesterday = dt.datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
     recent_data = AirKoreaData.objects.filter(stnfk__stationname=station_name).\
-        filter(datatime__range=(yesterday - timedelta(days=1), yesterday)).order_by('datatime')
+        filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)).order_by('datatime')
 
-    model = load_model('my_model.h5')
     x = list(recent_data.values_list('pm25value', flat=True))
-    x = np.array(x[-24:])
+    x = pd.Series(x[-24:])
+    x = x.interpolate()
+    x = np.array(x)
     x = x.reshape(-1, 24, 1)
 
-    y_pred = model.predict(x / 200)
-    y_pred = y_pred[0] * 200
-    y_pred = np.round(y_pred)
-    K.clear_session()
+    payload = {"instances": x.tolist()}
+    r = requests.post('http://localhost:8501/v1/models/lstm:predict', json=payload)
+    y_pred = json.loads(r.content.decode('utf-8'))
+    y_pred = y_pred['predictions'][0]
+    y_pred = [i * 200 for i in y_pred]
 
     forecast_dt = []
     for i in range(1, 7):
-        forecast_dt.append(yesterday + timedelta(hours=i))
+        forecast_dt.append(yesterday + dt.timedelta(hours=i))
 
     return render(request, "dashboard/detail.html", {'recent_data': recent_data, 'forecast': y_pred,
                                                      'forecast_dt' : forecast_dt})
 
 
 def list_table(request, status):
-    yesterday = dt.now().replace(microsecond=0, second=0, minute=0, hour=0)
+    yesterday = dt.datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
     if status == 'polluted':
-        stations_list = AirKoreaData.objects.filter(datatime__range=(yesterday - timedelta(days=1), yesterday)). \
+        stations_list = AirKoreaData.objects.filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)). \
             filter(khaigrade__gt=2).values('stnfk').distinct()
     elif status == 'clean':
-        stations_list = AirKoreaData.objects.filter(datatime__range=(yesterday - timedelta(days=1), yesterday)). \
+        stations_list = AirKoreaData.objects.filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)). \
             filter(khaigrade__lt=2).values('stnfk').distinct()
     elif status == 'failure':
-        stations_list = AirKoreaData.objects.filter(datatime__range=(yesterday - timedelta(days=1), yesterday)).\
+        stations_list = AirKoreaData.objects.filter(datatime__range=(yesterday - dt.timedelta(days=1), yesterday)).\
             values('stnfk').distinct()
         stations_list = AirKoreaStations.objects.exclude(id__in=stations_list).values('id')
     else :
